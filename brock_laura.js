e4698a131693e0244b7096a9684b7071130a4398
laura <laura.j.elias@gmail.com>
2019-08-13 15:06:29 -0500

    added functions as arguments 2 exercise

--- brock_laura.js
+++ brock_laura.js
@@ -1,17 +1,45 @@
+'use strict';
+
 function repeat(fn, n){
-    for (let i = 0; i < n; i++){
-        fn();
-    }
+  for (let i = 0; i < n; i++){
+    fn();
+  }
 }
 
 function hello(){
-    console.log('Hello World');
+  console.log('Hello World');
 }
 
 repeat(hello, 5);
 
 function goodbye(){
-    console.log('Goodbye Cruel World');
+  console.log('Goodbye Cruel World');
 }
 
 repeat(goodbye, 5);
+
+
+
+function filter(arr, fn) {
+  let newArray = [];
+  for (let i=0; i<arr.length; i++) {
+    if ( fn(arr[i]) ) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const predicate = (name) => {
+  return name[0] === 'R';
+};
+const filteredNames = filter(myNames, predicate);
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
