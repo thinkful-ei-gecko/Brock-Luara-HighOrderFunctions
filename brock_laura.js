9d62b1ae674679fc58b6692d3d0008e0b65fb6e0
Brock Boutwell <brockboutwell@gmail.com>
2019-08-13 16:40:44 -0400

    hazard warning

--- brock_laura.js
+++ brock_laura.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function repeat(fn, n){
+/* function repeat(fn, n){
   for (let i = 0; i < n; i++){
     fn();
   }
@@ -43,3 +43,23 @@ const filteredNames = filter(myNames, predicate);
 
 console.log(filteredNames) // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
+*/
+
+function hazardWarningCreator(typeOfWarning) {
+  let warningCounter = 0;
+  return location => {
+    warningCounter++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} ${warningCounter === 1 ? 'time' :'times'}.`);
+  };
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const godzilla = hazardWarningCreator('GODZIIIRRRRAAAAA');
+const alien = hazardWarningCreator('Aliens in the sky');
+
+rocksWarning('Downtown and EastTown');
+godzilla('The City and the Beach');
+alien('The Farm and the Crop circle');
