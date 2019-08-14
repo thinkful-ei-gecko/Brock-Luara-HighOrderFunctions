cf4c1beb47c7728ea2ae2f1a59160583a2e7c9ec
laura <laura.j.elias@gmail.com>
2019-08-13 22:53:47 -0500

    attempted final two problems, turtle steps is close, reduce is not good

--- brock_laura.js
+++ brock_laura.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* function repeat(fn, n){
+ function repeat(fn, n){
   for (let i = 0; i < n; i++){
     fn();
   }
@@ -43,7 +43,7 @@ const filteredNames = filter(myNames, predicate);
 
 console.log(filteredNames) // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
-*/
+
 
 function hazardWarningCreator(typeOfWarning) {
   let warningCounter = 0;
@@ -63,3 +63,65 @@ const alien = hazardWarningCreator('Aliens in the sky');
 rocksWarning('Downtown and EastTown');
 godzilla('The City and the Beach');
 alien('The Farm and the Crop circle');
+
+
+//TURTLE STEPS::::::::::
+
+let turtleSteps = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+
+let forwardLeftMoves = turtleSteps.filter(stepCombo => stepCombo[0] >= 0 && stepCombo[1] >= 0);
+
+let stepCounter = forwardLeftMoves.map(summingFunction);
+
+function summingFunction(move) {
+  return move[0] + move[1];
+}
+
+console.log(stepCounter);
+
+let forward = 0;
+let leftward = 0;
+
+let tallies = forwardLeftMoves.forEach(totalUp);
+
+function totalUp(move) {
+  forward += move[0];
+  leftward += move[1];
+  return `Forward move total: ${forward}, Leftward move total: ${leftward}`;
+}
+
+console.log(tallies);
+
+
+//REDUCE::::::: 
+//NOTE: W3 schools says that .reduce is a mathematical type of operation
+//so it's not clear how to use that with an array of strings...
+// this doesn't work, but some parts are probably good
+
+let input = "noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest";
+
+let newArray = function convertToArray(string) {
+  let stringArray = string.split(" ");
+  console.log(stringArray);
+  return stringArray;
+}
+convertToArray(input);
+console.log(newArray);
+
+function decoder(array) {
+  let initialValue = "";
+  newArray.reduce(word) {
+    if (word.length === 3) {
+      initialValue += " ";
+    }
+    else {
+      let endLetter = word.lastIndexOf();
+      initialValue += word[endLetter].toUpperCase();
+    }
+  }
+  console.log(initialValue);
+  return initialValue;
+};
+decoder(newArray);
+
+
