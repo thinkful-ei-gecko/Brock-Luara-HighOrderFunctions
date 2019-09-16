3b8507dbf4ab02703d9b101ae3fb49bc25cd72a7
Brock Boutwell <brockboutwell@gmail.com>
2019-09-16 10:02:11 -0400

    third attempt at reduce

--- brock_laura.js
+++ brock_laura.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
- function repeat(fn, n){
+/*function repeat(fn, n){
   for (let i = 0; i < n; i++){
     fn();
   }
@@ -41,11 +41,11 @@ const predicate = (name) => {
 };
 const filteredNames = filter(myNames, predicate);
 
-console.log(filteredNames) // => ['Rich', 'Ray']
+console.log(filteredNames) */
+// => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
-
-function hazardWarningCreator(typeOfWarning) {
+/*function hazardWarningCreator(typeOfWarning) {
   let warningCounter = 0;
   return location => {
     warningCounter++;
@@ -62,12 +62,11 @@ const alien = hazardWarningCreator('Aliens in the sky');
 
 rocksWarning('Downtown and EastTown');
 godzilla('The City and the Beach');
-alien('The Farm and the Crop circle');
-
+alien('The Farm and the Crop circle');*/
 
 //TURTLE STEPS::::::::::
 
-let turtleSteps = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+/*let turtleSteps = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
 
 let forwardLeftMoves = turtleSteps.filter(stepCombo => stepCombo[0] >= 0 && stepCombo[1] >= 0);
 
@@ -90,28 +89,25 @@ function totalUp(move) {
   return `Forward move total: ${forward}, Leftward move total: ${leftward}`;
 }
 
-console.log(tallies);
+console.log(tallies);*/
 
-
-//REDUCE::::::: 
+//REDUCE:::::::
 //NOTE: W3 schools says that .reduce is a mathematical type of operation
 //so it's not clear how to use that with an array of strings...
 // this doesn't work, but some parts are probably good
 
 // second attempt
-const input = "noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest";
+const input =
+  'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
 const newArr = input.split('');
 let decoder = newArr.reduce((decodeString, word) => {
-  if(word.length === 3){
+  if (word.length === 3) {
     return decodeString + '';
-
-  }
-  else {
+  } else {
     return decodeString + word[word.length - 1].toUpperCase();
   }
 }, '');
 console.log(decoder);
-
 
 // first attempt
 /* let newArray = function convertToArray(string) {
@@ -139,5 +135,3 @@ function decoder(array) {
 decoder(newArray);
 
 */
-
-
